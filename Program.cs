be4a2fdd860198f628867c636f8a476d6337cea4
Ahmet-Efdal Aktas <ahak00002@stud.uni-saarland.de>
2025-11-13 21:02:17 +0100

    Add Gitignore and Commandline Tool

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Text;
+
+string GetBitRepresentation(int i, int bitCount = 8)
+{
+    StringBuilder sb = new();
+    for (int shift = bitCount - 1; shift >= 0; shift--)
+    {
+        sb.Append(((i & (1 << shift)) != 0) ? '1' : '0');
+    }
+    return sb.ToString();
+}
+
+StringBuilder sb = new();
+for (int i = 0; i < 100; i++)
+{
+
+    sb.AppendLine($"{GetBitRepresentation(i)}\t---{i}\t---{i % 3} \t ---- {i % 3 == 1}");
+}
+
+File.WriteAllText(@"OUTPUT_PATH/Output.txt", sb.ToString());
